9e61130359b3ef88c9b959e485171e688738c95b
Raymond Rusk <rusk.raymond@gmail.com>
2015-07-06 16:20:31 -0700

    Oscar EMR Third Next Appointment Command-Line reporting tool

---
+++ ThirdNextAppointment.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# It is assumed that the mysql-connector jar file and the application are
+# in the current directory.  Note, it is possible to use command-line
+# configuration in "parm=value" format.  Command-line parameters override
+# parameters specified in the properties file.
+if [ -f ReportDate.class ]; then
+  rm ReportDate.class
+fi
+javac ReportDate.java
+if [ -f ThirdNextAppointment.class ]; then
+  rm ThirdNextAppointment.class
+fi
+javac ThirdNextAppointment.java
+java -cp ".:mysql-connector-java-5.1.28.jar" ThirdNextAppointment
